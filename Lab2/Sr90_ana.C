5ea9886662d2085184eebb24fc6e8a5b81d900db
Nick Tyler <tylern@canisius.edu>
2018-04-10 13:57:54 -0400

    Add comments to code

--- Lab2/Sr90_ana.C
+++ Lab2/Sr90_ana.C
@@ -1,19 +1,23 @@
 double poison(double *x, double *par) { return par[0] * TMath::Poisson(x[0], par[1]); }
 
 void Sr90_ana() {
-  const char *fin = "5ms_1000s.root";
-  TChain *chain = new TChain("Sr90");
-  chain->Add(fin);
-  TF1 *pois = new TF1("pois", poison, 0, 10, 2);
-
   gStyle->SetOptStat(111);
   gStyle->SetOptFit(111);
 
+  // Input filename
+  const char *fin = "5ms_1000s.root";
+  TChain *chain = new TChain("Sr90");
+  chain->Add(fin);
+  // This creates a Poisson fitting function from the function created above
+  TF1 *pois = new TF1("pois", poison, 0, 10, 2);
+
+  // Get values from the file
   int _x1;
   chain->SetBranchAddress("x1", &_x1);
 
   int num_of_events = (int)chain->GetEntries();
   for (int current_event = 0; current_event < num_of_events; current_event++) {
+    // Get current event and print out the event rate
     chain->GetEntry(current_event);
     std::cout << "Count rate for event\t" << current_event << ":\t" << _x1 << std::endl;
   }
--- Lab2/plot_points.C
+++ Lab2/plot_points.C
@@ -2,9 +2,13 @@ double x[3] = {1.0, 2.0, 3.0};
 double y[3] = {3.0, 2.0, 3.5};
 
 void plot_points() {
-  gStyle->SetOptStat(111);
-  gStyle->SetOptFit(111);
+  gStyle->SetOptStat(1111);
+  gStyle->SetOptFit(1111);
+  // Creates a graph with 3 points from the arrays X and Y
   TGraph *gr = new TGraph(3, x, y);
+  // Fit the graph with a polyinomial of order 2
+  // I.E. y = p0 * x^2 + p1 * x + p2
   gr->Fit("pol2");
-  gr->Draw("AC*");
+  // Draws all the points with a * marker
+  gr->Draw("A*");
 }
